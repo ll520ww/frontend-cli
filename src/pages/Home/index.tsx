4ab77420e1dff058099307d623e86e01496303b0
zcy <13262115921@163.com>
2023-11-03 14:28:14 +0800

    feat:项目初始化

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,8 @@
-import Guide from '@/components/Guide';
-import {trim} from '@/utils/format';
 import {PageContainer} from '@ant-design/pro-components';
-import {useModel} from '@umijs/max';
 import styles from './index.less';
 import {ForControl} from "@/components/control";
 
 const HomePage: React.FC = () => {
-    const {name} = useModel('global');
     return (
         <PageContainer ghost>
             <div className={styles.container}>
